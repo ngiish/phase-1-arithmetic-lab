67b38b1eec73432e7dad4e4ba2438381aba13955
Isaac <govegeku00@gmail.com>
2023-10-16 13:59:28 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+ let num1= 31
+let num2 = 2
 
-// Write your code here
+let multiply = (num1 * num2)
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3= 28
+let num4 =6
+let mod = num3 % num4
+
+let numArray= [20,14,12,16,18,9,5]
+let max = Math.max(...numArray)
